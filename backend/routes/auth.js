b8b8e6203db8450730722294494c115e688389b8
Iezstalker <106069608+Iezstalker@users.noreply.github.com>
2023-12-27 20:49:27 +0530

    Hashing passwords using bcryptjs in NodeJs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'NileshKumar$G'
 
 //Create a user using: POST: "/api/auth/createUser". No login required.
 router.post('/createUser',[
@@ -26,9 +30,11 @@ router.post('/createUser',[
 
 
   //Create a new user
+  const salt = await bcrypt.genSalt(10)
+  const SecPass = await bcrypt.hash(req.body.password, salt) 
   user = await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: SecPass,
       email: req.body.email,
     })
   //   .then(user => res.json(user))
@@ -36,7 +42,14 @@ router.post('/createUser',[
   // res.json({error: 'Please enter a unique value for email', message: err.message})})
 
 
-  res.json(user)
+  const data ={
+    user:{
+      id:user.id
+    }
+  }
+
+  const authToken = jwt.sign(data, JWT_SECRET)
+  res.json({authToken})
   }
    
   catch(error){
@@ -47,5 +60,16 @@ router.post('/createUser',[
   // res.send("hello")
 })
 
+
+//Create a user using: POST: "/api/auth". Doesn't require Auth
+router.get('/', (req, res)=>{
+
+  console.log(req.body)
+  const user = User(req.body)
+  user.save()
+  console.log(user)
+  res.send(req.body);
+  // res.send("hello")
+})
 
 module.exports = router
