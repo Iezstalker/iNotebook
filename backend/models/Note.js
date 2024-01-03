a97bf532903e8739b11c191d413665cfe151a225
Iezstalker <106069608+Iezstalker@users.noreply.github.com>
2024-01-03 23:10:32 +0530

    Fetching all notes & adding the notes

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,29 +1,29 @@
 // import mongoose,{Schema} from "mongoose";
 const mongoose = require('mongoose')
-const {Schema} = mongoose
+const { Schema } = mongoose
 
 const NotesSchema = new Schema({
-    user:{
+    user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     },
-title:{
-    type:String,
-    required:true
-},
-description:{
-    type:String,
-    required:true
-},
-tag:{
-    type:String,
-    default:"General"
-},
-date:{
-    type:Date,
-    default:Date.now
-}
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        default: "General"
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
-  );
+}
+);
 
-module.exports = mongoose.model('Notes',NotesSchema);
+module.exports = mongoose.model('Notes', NotesSchema);
